c5e9fbabe686c6236511af370292ad41a35242fd
Mostepha Amine Bdeira <49349658+MABDesigns@users.noreply.github.com>
2022-08-28 05:10:02 +0100

    successfully completed

---
+++ index.php
@@ -0,0 +1,174 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JohnathanSpecter Portfolio</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"/>
+    <link rel="stylesheet" href="src/css/style.css">
+    <link rel="stylesheet" href="src/css/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/css/bootstrap.min.css"/>
+</head>
+<body>
+    <br>
+    <br>
+ <!-----------------------------------------------
+    -------------------------------------------------
+    ----------------- Start of navbar -----------------
+    -------------------------------------------------
+    ------------------------------------------------>
+    <nav class="navbar navbar-expand-lg ">
+  <div class="container">
+    <a class="navbar-brand" href="#">JohnathanSpecter</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav mx-auto">
+        <a class="nav-link " aria-current="page" href="#">Articles</a>
+        <a class="nav-link" href="#">Chats</a>
+        <a class="nav-link" href="#">Awards</a>
+        <a class="nav-link" href="#">About</a>
+      </div>
+      <button type="button" class="btn btn-primary">Get in touch</button>
+    </div>
+  </div>
+</nav>
+    <!-----------------------------------------------
+    -------------------------------------------------
+    ----------------- End of navbar -----------------
+    -------------------------------------------------
+    ------------------------------------------------>
+
+
+ <!-----------------------------------------------
+    -------------------------------------------------
+    ----------------- Start of content -----------------
+    -------------------------------------------------
+    ------------------------------------------------>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Helping companies build<br> better, scalable software.</h1>
+                <p>Award-winning web developer and author, with over 15+ years of working<br> experience with Fortune 500 companies like Apple, Google, Facebook, and <br>more.</p>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="row row-lg-col-5">
+            <div class="col"><img src="Assets/Logos/Walmart.svg"></div><br>
+            <div class="col"><img src="Assets/Logos/JP Morgan.svg"></div><br>
+            <div class="col"><img src="Assets/Logos/Visa.svg"></div><br>
+            <div class="col"><img src="Assets/Logos/Tinder.svg"></div><br>
+            <div class="col"><img src="Assets/Logos/Samsung.svg"></div><br>
+            <div class="col"><img src="Assets/Logos/Verizon.svg"></div><br>
+        </div>
+    </div>
+     <!-----------------------------------------------
+    -------------------------------------------------
+    ----------------- End of content -----------------
+    -------------------------------------------------
+    ------------------------------------------------>
+
+     <!-----------------------------------------------
+    -------------------------------------------------
+    ----------------- Start of cards -----------------
+    -------------------------------------------------
+    ------------------------------------------------>
+    <br>
+    <br>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="image-cd">
+                        <img src="Assets/Spense.png">
+                    </div>
+                    <div class="title-cd">
+                        <h2>Spense.com  →</h2>
+                    </div>
+                    <div class="desc-cd">
+                        <p>Rethinking the way writers get paid, an open-source platform designed to help writers focus more on writing, and
+                            less on when and how they'll get paid. Project in collaboration with Codewell.cc
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+            <div class="card">
+                    <div class="image-cd">
+                        <img src="Assets/YelpCamp.png">
+                    </div>
+                    <div class="title-cd">
+                        <h2>YelpCamp.com  →</h2>
+                    </div>
+                    <div class="desc-cd">
+                        <p>Allowing backpack travelers to perfectly plan their trip through an open-source similar to TripAdvisor. With over 1m MAU, YelpCamp has been
+                            the crowd's favorite in 2021.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--------------------------------------------->
+    <div class="container-fluid"id="desc">
+    <div class="container" id="desc">
+        <div class="row">
+            <div class="col">
+                <h2>A co-founder at one of the world's largest communities</h2>
+                <p>The combined experience i have working at the top Fortune 500 companies has allowed me to developer a skillest
+                    that helps me in not just developement, but in every aspect of any business. 
+                </p>
+                <p>I'm proud to announce that i'am working now at one of the world's largest communities teaching young minds About
+                    how to sell yourself as a developer and open them to a whole new world of opportunities.
+                </p>
+            </div>
+            <div class="col">
+                <p>As a developer, you have everything available to you and all that's holding you back is yourself.</p>
+                <p>A quote i live by perfectly illustrates what i mean.</p>
+                <p>"How big would you dream, if you <strong>knew</strong>you wouldn't fall?"<br>You've already gone through the hardest
+                    parts being a developer, it's time to elevate your skills and become a leader in something you're passionate about.</p>
+                <p>I'm happy to chat over coffee some time about how i can help your company.</p>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!------------------------------------------>
+    <div class="container-fluid" id="desc2">
+        <div class="container" id="desc2">
+            <div class="row">
+                <div class="col">
+                    <h2>Interested in working with me?</h2>
+                    <p>I'm active on all social media platforms listed below, but you<br> can also send me an email and i will get back to you within<br> 24-28 hours.</p>
+                    <button type="button" class="btn btn-primary">Get in touch</button>
+                </div>
+            </div>
+        </div>
+    </div>
+   <!------------------------------------------->
+   <div class="container-fluid" id="desc3">
+        <div class="container" id="desc3">
+                 <a class="navbar-brand" href="#">JohnathanSpecter</a>
+                
+                        <div class="float-end">
+                            <img src="Assets/Social Icons/Twitter.svg">
+                            <img src="Assets/Social Icons/LinkedIn.svg">
+                            <img src="Assets/Social Icons/Github.svg">
+                        </div>  
+            
+        </div>
+   </div> 
+<!--JQUERY + BOOTSTRAP JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.min.js"></script>
+</body>
